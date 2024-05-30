b1405b44690f9bdf2eb073a0b5604ac790271248
Brian Hamilton <bdhamilton@gmail.com>
2024-05-30 11:53:31 -0400

    Move planning docs to backup folder

--- databasedesign.md
+++
@@ -1,4 +0,0 @@
-# Database design
-
-I think this should be doable with two simple databases
-
--- displayrecords.js
+++
@@ -1,6 +0,0 @@
-/***
- * I need functions that will:
- * - update the streak counter for the current student (student-facing)
- * - print the weekly trend for a current student (teacher-facing)
- * - print the calendar (both)
- */
